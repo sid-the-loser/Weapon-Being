6d5b0e9e4ed03652d72a32454b8b165ae601c385
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-01-19 02:32:43 -0500

    remaking sidengine

--- src/sidengine/__init__.py
+++ src/sidengine/__init__.py
@@ -1,4 +1,6 @@
 """SidEngine is a custom engine made by SidTheLoser.
 It's a work in progress, and it only does the jobs it's meant to do as per the games requirements.
 I will release this as a full-blown engine later.
+
+This engine is optimised for HTML exports.
 """
--- src/sidengine/app.py
+++ src/sidengine/app.py
@@ -1,12 +1,18 @@
 import pygame
 import asyncio
+from sidengine.colors import *
 
 class App:
     def __init__(self):
         self.window = None
+
         self.window_size = (600, 600)
         self.window_title = "Untitled SidEngine Game"
+
         self.running = True
+
+        self.scenes = []
+        self.current_scene_index = 0
 
     def init(self):
         pygame.init()
@@ -16,9 +22,32 @@ class App:
 
     async def run(self):
         while self.running:
+            self.window.fill(BLACK)
+
+            if len(self.scenes) > 0:
+                self.scenes[0].events()
+                self.scenes[0].update()
+                self.scenes[0].draw()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
             pygame.display.flip()
             await asyncio.sleep(0)
+
+class Scene:
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def events(self):
+        pass
