8d6bb62d933d012acc3f346e17472cb272b64d6c
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-01-19 13:51:29 -0500

    a lot was done, can't even document it

--- src/sidengine/app.py
+++ src/sidengine/app.py
@@ -10,8 +10,12 @@ class App:
         self.window_size = (600, 600)
         self.window_title = "Untitled SidEngine Game"
 
+        self.clock = pygame.time.Clock()
+        self.dt = 1
+
         self.running = True
 
+        self.scenes: tuple[Scene]
         self.scenes = []
         self.current_scene_index = 0
 
@@ -21,18 +25,28 @@ class App:
         self.window = pygame.display.set_mode(self.window_size)
         pygame.display.set_caption(self.window_title)
 
+    def change_scene(self, scene_index):
+        self.scenes[self.current_scene_index].init_ran = False
+        self.current_scene_index = scene_index
+
     async def run(self):
         while self.running:
             self.window.fill(BLACK)
+            self.dt = self.clock.tick() / 1000
 
             if len(self.scenes) > 0:
-                self.scenes[0].events()
-                self.scenes[0].update()
-                self.scenes[0].draw()
+                if not self.scenes[self.current_scene_index].init_ran:
+                    self.scenes[self.current_scene_index].init()
+                    self.scenes[self.current_scene_index].init_ran = True
+                self.scenes[self.current_scene_index].update()
+                self.scenes[self.current_scene_index].draw()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+
+                if len(self.scenes) > 0:
+                    self.scenes[self.current_scene_index].events(event)
 
             pygame.display.flip()
             await asyncio.sleep(0)
@@ -40,7 +54,7 @@ class App:
 
 class Scene:
     def __init__(self):
-        pass
+        self.init_ran = False
 
     def init(self):
         pass
@@ -51,5 +65,5 @@ class Scene:
     def draw(self):
         pass
 
-    def events(self):
+    def events(self, event: pygame.event):
         pass
