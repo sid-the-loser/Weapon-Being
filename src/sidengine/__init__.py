ba58e0ed9a0111aa3e5190e2135a089d1a6ab08f
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-01-19 01:32:48 -0500

    starting from scratch in love2d

--- src/sidengine/__init__.py
+++
@@ -1,4 +0,0 @@
-"""SidEngine is a custom engine built on top of pygame with a similar code styles to Kivy.
-This engine was built by Sidharth to help with future projects. He will release this engine as a custom module that you
-import and use, but that's still a WIP.
-"""
